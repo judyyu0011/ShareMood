0a335fde4753087d248ef33c24070df605457c10
jjwong783 <jjwong783@gmail.com>
2020-12-06 13:16:21 -0800

    Base for stickynotes

---
+++ src/MoodBoard.js
@@ -0,0 +1,61 @@
+
+
+export class MoodBoard {
+    avaliablePos = [
+        { x: 100, y: 100 },
+        { x: 1000, y: 100 },
+        { x: 795, y: 250 }];
+    stickyColours = { //dictionary of moods and their respective stickynot colour
+        "happy": "yellow",
+        "sad": "blue",
+        "anxious": "purple",
+        "meh": "green",
+        "excited": "orange"
+    }
+
+    notes; //list of stickynotes JSON
+    takenPos; //list of taken positions 
+
+
+    constructor(){
+        this.notes = [];
+        this.takenPos = [];
+    }
+
+    // return list of all notes on board
+    getBoard() {
+        return notes;
+    }
+
+    // takes in a JSON object containing mood and message info from frontend form 
+    generateSticky(info) {
+        let sticky = {};
+        sticky.colour = getStickyColour(info.mood);
+        sticky.message = info.message;
+        
+        let pos = sticky.getStickyPosition();
+        if (pos !== false){
+            sticky.posx = pos.x;
+            sticky.posy = pos.y;
+        } else {
+            // throw error here??? We want to throw some 400 error or something to FE
+        }
+    }
+
+    // takes in String mood
+    // returns String colour
+    getStickyColour(mood){
+        return this.stickyColours[mood];
+    }
+
+    // gets position Object from avaliable positions
+    // if no avaliable position, return false
+    getStickyPosition(){
+        let pos; //JSON with x & y coordinates
+        if (this.takenPos.length < this.avaliablePos.length){
+            pos = this.avaliablePos.pop();
+        } else {
+            return false; // no more avaliable positions
+        }
+    }
+}

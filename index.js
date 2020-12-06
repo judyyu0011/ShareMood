cff5e381b4e20336c811f368a0bef2eb9bb3ade9
Judy Yu <judyyu0011@gmail.com>
2020-12-06 15:30:12 -0800

    change structure to ejs

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-const { readFile } = require('fs');
 
 app.use(express.static(__dirname));
+app.set('view engine', 'ejs')
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+    var data = {mood: "happy", description: "this is happy description", color: "red"}
+
+    res.render('home', {data:data});
+});
+
+app.get('/form', (req, res) => {
+    res.render('form');
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(`http://localhost:3000`));

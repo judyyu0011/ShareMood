824e0e171be36f0c30759abc7d34b26c95eb02c4
Judy Yu <judyyu0011@gmail.com>
2020-12-20 16:17:14 -0800

    update form validations

--- public/app.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-    var mood = document.forms["mood-form"]["mood"].value;
-    if (mood=="") {
-        alert("Please select a mood.")
-        console.log("alert");
-    } else {
-        console.log("what");
-    }
-}

9770322c461a01a79c477546299d0b8ae9028a63
Judy Yu <judyyu0011@gmail.com>
2020-12-24 12:00:39 -0800

    load stickies every time board displays

--- public/board.js
+++ public/board.js
@@ -1,50 +1,45 @@
 $(document).ready(()=>{
 
-    var length = sessionStorage.getItem("stickies-length");
+    // load board
+    $.get('/board', function(data) {
+        for (var i = 0; i < data.length; i++) {
+            var stickyObject = data[i];
 
-    var stickiesContainer = document.getElementById('stickies');
+            var stickyMes = stickyObject.message;
+            var stickyColor = stickyObject.colour;
+            var stickyPosX = stickyObject.posx;
+            var stickyPosY = stickyObject.posy;
 
-    // loop over sticky data stored in session storage
-    // place and style each sticky
-    for (var i = 0; i < length; i++){
+            var stickyVisual = document.createElement('div');
+            stickiesContainer.appendChild(stickyVisual);
+            stickyVisual.innerHTML = stickyMes;
 
-        var stickyObject = JSON.parse(sessionStorage.getItem(i));
-        console.log(stickyObject);
+            const stickySize = 50;
 
-        var stickyMes = stickyObject.message;
-        var stickyColor = stickyObject.colour;
-        var stickyPosX = stickyObject.posx;
-        var stickyPosY = stickyObject.posy;
+            stickyVisual.style.position = 'absolute';
+            stickyVisual.style.left = stickyPosX + 'px';
+            stickyVisual.style.top = stickyPosY + 'px';
+            stickyVisual.style.width = stickySize + 'px';
+            stickyVisual.style.height = stickySize + 'px';
+            stickyVisual.style.fontSize = '12px';
 
-        var stickyVisual = document.createElement('div');
-        stickiesContainer.appendChild(stickyVisual);
-        stickyVisual.innerHTML = stickyMes;
-
-        const stickySize = 50;
-
-        stickyVisual.style.position = 'absolute';
-        stickyVisual.style.left = stickyPosX + 'px';
-        stickyVisual.style.top = stickyPosY + 'px';
-        stickyVisual.style.width = stickySize + 'px';
-        stickyVisual.style.height = stickySize + 'px';
-        stickyVisual.style.fontSize = '12px';
-
-        // set sticky image based on colour
-        if (stickyColor == 'purple') {
-            stickyVisual.style.backgroundImage = "url('images/sticky-purple.png')";
-            stickyVisual.style.backgroundSize = stickySize + "px " + stickySize + "px";
-        } else if (stickyColor == 'blue') {
-            stickyVisual.style.backgroundImage = "url('images/sticky-blue.png')";
-            stickyVisual.style.backgroundSize = stickySize + "px " + stickySize + "px";
-        } else if (stickyColor == 'green') {
-            stickyVisual.style.backgroundImage = "url('images/sticky-green.png')";
-            stickyVisual.style.backgroundSize = stickySize + "px " + stickySize + "px";
-        } else if (stickyColor == 'yellow') {
-            stickyVisual.style.backgroundImage = "url('images/sticky-yellow.png')";
-            stickyVisual.style.backgroundSize = stickySize + "px " + stickySize + "px";
-        } else if (stickyColor == 'orange') {
-            stickyVisual.style.backgroundImage = "url('images/sticky-orange.png')";
-            stickyVisual.style.backgroundSize = stickySize + "px " + stickySize + "px";
-        } 
-    }
+            // set sticky image based on colour
+            if (stickyColor == 'purple') {
+                stickyVisual.style.backgroundImage = "url('images/sticky-purple.png')";
+                stickyVisual.style.backgroundSize = stickySize + "px " + stickySize + "px";
+            } else if (stickyColor == 'blue') {
+                stickyVisual.style.backgroundImage = "url('images/sticky-blue.png')";
+                stickyVisual.style.backgroundSize = stickySize + "px " + stickySize + "px";
+            } else if (stickyColor == 'green') {
+                stickyVisual.style.backgroundImage = "url('images/sticky-green.png')";
+                stickyVisual.style.backgroundSize = stickySize + "px " + stickySize + "px";
+            } else if (stickyColor == 'yellow') {
+                stickyVisual.style.backgroundImage = "url('images/sticky-yellow.png')";
+                stickyVisual.style.backgroundSize = stickySize + "px " + stickySize + "px";
+            } else if (stickyColor == 'orange') {
+                stickyVisual.style.backgroundImage = "url('images/sticky-orange.png')";
+                stickyVisual.style.backgroundSize = stickySize + "px " + stickySize + "px";
+            } 
+        }
+    });
 });
